e87f2d5bfb6fb61d12ebfcacf8bc69679f6a1380
Jeffery Cosby <jeffery.cosby@omes.ok.gov>
2020-10-22 12:34:58 -0500

    getAllBooks, getAllUsers

--- src/server/shared.js
+++ src/server/shared.js
@@ -1,5 +1,6 @@
 const jsonfile = require("jsonfile");
 const users = "./database/users.json";
+const inventory = "./database/books.json";
 const bcrypt = require("bcrypt");
 
 exports.getUserByUsername = async function (username) {
@@ -18,4 +19,23 @@ exports.isEmptyObject = (object) => Object.entries(object).length === 0;
 
 exports.isPasswordCorrect = async function (key, password) {
   return bcrypt.compare(password, key).then((result) => result);
+};
+
+exports.getAllBooks = async function() {
+  try {
+    return await jsonfile.readFile(inventory);
+  }
+  catch(err) {
+    console.log("Error reading books: ", err);
+  }  
+};
+
+exports.getAllUsers = async function() {
+  try {
+    const allUsers = await jsonfile.readFile(users);
+    let updatedUsers = [];
+  }
+  catch(err) {
+    console.log("Error reading users: ", err);
+  }  
 };
